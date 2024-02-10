236949220fb11bcfc1c80d1c521bb63d146c1e5c
Daniel Granados <20200008@ricaldone.edu.sv>
2024-02-10 17:32:41 -0600

    Hice que la info apareciera al presionar un boton, agregue su codigo html, css y js

---
+++ js/script.js
@@ -0,0 +1,84 @@
+//Esta función hace que ningun div (ningun empresario) aparezca al cargar la página
+window.onload = function() {
+    ocultarDivs();
+}
+
+function ocultarDivs() {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.getElementById("div7").style.display = "none";
+    document.getElementById("div8").style.display = "none";
+    document.getElementById("div9").style.display = "none";
+    document.getElementById("div10").style.display = "none";
+}
+
+/*Estas funciones hacen que cuando se presione el boton correspondiente,
+aparezca el empresario respectivo.*/
+function elon() {
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.getElementById("div7").style.display = "none";
+    document.getElementById("div8").style.display = "none";
+    document.getElementById("div9").style.display = "none";
+    document.getElementById("div10").style.display = "none";
+}
+
+function bill() {
+    document.getElementById("div2").style.display = "block";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.getElementById("div7").style.display = "none";
+    document.getElementById("div8").style.display = "none";
+    document.getElementById("div9").style.display = "none";
+    document.getElementById("div10").style.display = "none";
+}
+
+function steve() {
+    document.getElementById("div3").style.display = "block";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.getElementById("div7").style.display = "none";
+    document.getElementById("div8").style.display = "none";
+    document.getElementById("div9").style.display = "none";
+    document.getElementById("div10").style.display = "none";
+}
+
+function mark() {
+    document.getElementById("div4").style.display = "block";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.getElementById("div7").style.display = "none";
+    document.getElementById("div8").style.display = "none";
+    document.getElementById("div9").style.display = "none";
+    document.getElementById("div10").style.display = "none";
+}
+
+function mark() {
+    document.getElementById("div5").style.display = "block";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.getElementById("div7").style.display = "none";
+    document.getElementById("div8").style.display = "none";
+    document.getElementById("div9").style.display = "none";
+    document.getElementById("div10").style.display = "none";
+}
